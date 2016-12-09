846c4adc940a57e3784879fde57622b86434556c
s170032200080 <swe.swethamurali@gmail.com>
2016-12-09 07:30:08 -0800

    Add files via upload

--- mobile/src/main/java/com/niit/controller/appController.java
+++ mobile/src/main/java/com/niit/controller/appController.java
@@ -1,5 +1,16 @@
 package com.niit.controller;
-@Controller
-public class appController {
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+
+public class appController{
+	@RequestMapping("/")
+	
+	public String showIndex()
+	{
+		return"index";
+	}
 }
